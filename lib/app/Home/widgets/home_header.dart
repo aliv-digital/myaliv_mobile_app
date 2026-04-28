55a5c5e9144f51df8be1591fad4aa87dc7f79acd
MamunWSIT <mamunwsit@github.com>
2026-04-29 01:58:03 +0600

    fix update emails and password screen

--- lib/app/Home/widgets/home_header.dart
+++ lib/app/Home/widgets/home_header.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:core/core.dart';
 import 'package:myaliv_mobile_app/app/Aliv-Mobile/account-information/cubit/account_info_cubit.dart';
+import 'package:myaliv_mobile_app/app/Aliv-Mobile/account-information/cubit/account_info_state.dart';
 import 'package:myaliv_mobile_app/app/Home/home/data/home_ui_config.dart';
 import 'package:myaliv_mobile_app/app/Home/my-limits/device-limits/cubit/device_limits_cubit.dart';
 import 'package:myaliv_mobile_app/app/Home/my-limits/device-limits/cubit/device_limits_state.dart';
@@ -56,18 +57,25 @@ class HomeHeader extends StatelessWidget {
           const SizedBox(height: 25),
           BlocBuilder<DeviceLimitsCubit, DeviceLimitsState>(
             bloc: instance<DeviceLimitsCubit>(),
-            builder: (context, state) {
-              final email = instance<AccountInfoCubit>().state.accountInfo?.email ?? '';
-              final name = state.fullName ?? _nameFromEmail(email);
-              return Text(
-                'welcome back, $name',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: const Color(0xFFF1F1F8),
-                  fontSize: 16,
-                  fontFamily: 'CircularPro',
-                  fontWeight: FontWeight.w500,
-                ),
+            builder: (context, deviceState) {
+              return BlocBuilder<AccountInfoCubit, AccountInfoState>(
+                bloc: instance<AccountInfoCubit>(),
+                buildWhen: (previous, current) =>
+                    previous.accountInfo?.email != current.accountInfo?.email,
+                builder: (context, accountState) {
+                  final email = accountState.accountInfo?.email ?? '';
+                  final name = deviceState.fullName ?? _nameFromEmail(email);
+                  return Text(
+                    'welcome back, $name',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: const Color(0xFFF1F1F8),
+                      fontSize: 16,
+                      fontFamily: 'CircularPro',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  );
+                },
               );
             },
           ),
