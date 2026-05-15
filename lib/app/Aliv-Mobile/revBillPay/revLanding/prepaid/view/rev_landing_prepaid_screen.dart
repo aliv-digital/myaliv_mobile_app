326510962557c36eab89dfdeebdefb7e28bdd5af
MamunWSIT <mamunwsit@github.com>
2026-05-15 18:47:03 +0600

    rev bill payment screen impl

--- lib/app/Aliv-Mobile/revBillPay/revLanding/prepaid/view/rev_landing_prepaid_screen.dart
+++ lib/app/Aliv-Mobile/revBillPay/revLanding/prepaid/view/rev_landing_prepaid_screen.dart
@@ -1,28 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter/services.dart';
 import 'package:myaliv_mobile_app/app/Aliv-Mobile/revBillPay/revLanding/prepaid/theme/rev_landing_prepaid_theme.dart';
 import 'package:myaliv_mobile_app/app/Aliv-Mobile/revBillPay/revLanding/prepaid/widgets/rev_landing_back_button.dart';
 import 'package:myaliv_mobile_app/app/Aliv-Mobile/revBillPay/revLanding/prepaid/widgets/rev_landing_bottom_decoration.dart';
 import 'package:myaliv_mobile_app/app/Aliv-Mobile/revBillPay/revLanding/prepaid/widgets/rev_landing_choice_button.dart';
 import 'package:myaliv_mobile_app/app/Aliv-Mobile/revBillPay/revLanding/prepaid/widgets/rev_landing_hero_image.dart';
-import 'package:myaliv_mobile_app/router/app_routes.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class RevLandingPrepaidScreen extends StatelessWidget {
   const RevLandingPrepaidScreen({super.key});
 
+  static const _payAsGuestUrl = 'https://my.rev.bs/guest/payment';
+  static const _loginToPayUrl = 'https://my.rev.bs/';
+
+  Future<void> _openUrl(String url) async {
+    final uri = Uri.parse(url);
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: RevLandingPrepaidTheme.background,
-      body: Stack(
-        children: [
-          Positioned(
-            right: 0,
-            bottom: 0,
-            child: const RevLandingBottomDecoration(),
-          ),
-          SafeArea(
-            child: LayoutBuilder(
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        backgroundColor: RevLandingPrepaidTheme.background,
+        extendBodyBehindAppBar: true,
+        body: Stack(
+          children: [
+            Positioned(
+              right: 0,
+              bottom: 0,
+              child: const RevLandingBottomDecoration(),
+            ),
+            LayoutBuilder(
               builder: (context, constraints) {
                 return SingleChildScrollView(
                   physics: const ClampingScrollPhysics(),
@@ -50,8 +64,7 @@ class RevLandingPrepaidScreen extends StatelessWidget {
                           Center(
                             child: RevLandingChoiceButton(
                               label: 'Pay as a Guest',
-                              onPressed: () =>
-                                  context.push(AppRoutes.guestPayBill),
+                              onPressed: () => _openUrl(_payAsGuestUrl),
                             ),
                           ),
                           const SizedBox(
@@ -60,7 +73,7 @@ class RevLandingPrepaidScreen extends StatelessWidget {
                           Center(
                             child: RevLandingChoiceButton(
                               label: 'Log in to Pay',
-                              onPressed: () => context.push(AppRoutes.logIn),
+                              onPressed: () => _openUrl(_loginToPayUrl),
                             ),
                           ),
                           const Expanded(child: SizedBox.shrink()),
@@ -71,13 +84,13 @@ class RevLandingPrepaidScreen extends StatelessWidget {
                 );
               },
             ),
-          ),
-          Positioned(
-            top: RevLandingPrepaidTheme.backButtonTop,
-            left: RevLandingPrepaidTheme.backButtonLeft,
-            child: SafeArea(child: const RevLandingBackButton()),
-          ),
-        ],
+            Positioned(
+              top: RevLandingPrepaidTheme.backButtonTop,
+              left: RevLandingPrepaidTheme.backButtonLeft,
+              child: SafeArea(child: const RevLandingBackButton()),
+            ),
+          ],
+        ),
       ),
     );
   }
